f8b7ae8553f8d61fa352a58996e9d83661340674
Paulo Sousa <paulo.sousa@farfetch.com>
2019-11-11 20:23:05 +0000

    construction page added

---
+++ src/components/ConstructionsPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Facebook from '../assets/facebook.svg';
+
+const ConstructionPage = () => {
+  return (
+    <>
+      <h1>VOLUNTEI</h1>
+      <p>Opa! Que legal você por aqui!</p>
+      <p>Estamos trabalhando para te proporcionar melhor experiência.</p>
+      <p>Enquanto fazemos isso, você pode nos acompanhar nas redes sociais e ficar por dentro de tudo.</p>
+      <img src={../ assets / facebook.svg} />
+    </>
+  );
+};
+
+export default ConstructionPage;
