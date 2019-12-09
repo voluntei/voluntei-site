aaeda386720c00d17d8447ca25c2983eb3a536ee
Paulo Sousa <paulo.sousa@farfetch.com>
2019-12-09 14:40:02 +0000

    feat: Add links to page

--- src/components/ContructionPage/ConstructionsPage.js
+++ src/components/ContructionPage/ConstructionsPage.js
@@ -8,9 +8,9 @@ import BgAmarelo from "../../assets/bgAmarelo.svg";
 const ConstructionPage = () => {
   return (
     <>
-      <FaixaConstrucao style={{ width: '36rem', position: 'fixed', top: '0', left: '0' }} />
-      <Ceu style={{ width: '75rem', position: 'fixed', top: '0', right: '0' }} />
-      <BgAmarelo style={{ position: 'fixed', bottom: '0', left: '0' }} />
+      <FaixaConstrucao style={{ width: '36rem',height: 'auto', position: 'fixed', top: '0', left: '0' }} />
+      <Ceu style={{ width: '75rem', height: 'auto',position: 'fixed', top: '0', right: '0' }} />
+      <BgAmarelo style={{ width: '92rem', height: 'auto',position: 'fixed', bottom: '0', left: '0' }} />
       <div style={{ height: '100%', display: 'flex', alignItems: 'center', zIndex: '1' }}>
         <Text />
       </div>
--- src/components/SocialNetwork.js
+++ src/components/SocialNetwork.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import Facebook from "../assets/fb.svg";
+import Discord from "../assets/discord.svg";
+import Twitter from "../assets/tw.svg";
+import Linkedin from "../assets/in.svg";
+import Instagram from "../assets/ig.svg";
 
 const SocialNetwork = (props) => {
-    console.log('props', props)
-    const svgWidth = '4rem';
-    const svgRightMargin = '1.2rem';
+    const linkProps = {
+        links: {
+            Discord: 'https://www.discord.gg/UmfS2dJ',
+            Facebook: 'https://www.facebook.com/voluntei/',
+            Instagram: 'https://www.instagram.com/voluntei/',
+            Twitter: 'https://twitter.com/gatsbyjs',
+            Linkedin: 'https://www.linkedin.com/company/voluntei/',
+        },
+        style:{
+            width: '5.7rem',
+            marginRight: '1.2rem',
+            height: 'auto',
+        },
+    }
     return (
         <div className={props.theme} style={{ marginTop: '2rem' }}>
-            <Facebook style={{ width: svgWidth, marginRight: svgRightMargin }} />
+            <a href={linkProps.links.Discord} target="_blank" rel="noopener noreferrer">
+                <Discord style={linkProps.style} />
+            </a>
+            <a href={linkProps.links.Facebook} target="_blank" rel="noopener noreferrer">
+                <Facebook style={linkProps.style} />
+            </a>
+            <a href={linkProps.links.Instagram} target="_blank" rel="noopener noreferrer">
+                <Instagram style={linkProps.style} />
+            </a>
+            <a href={linkProps.links.Twitter} target="_blank" rel="noopener noreferrer">
+                <Twitter style={linkProps.style} />
+            </a>
+            <a href={linkProps.links.Linkedin} target="_blank" rel="noopener noreferrer">
+                <Linkedin style={linkProps.style} />
+            </a>
         </div>
     );
 };
